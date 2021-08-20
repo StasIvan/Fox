69b3da673e4929f7e7552c7bcb8ab1a4636397b3
StasIvan <vanyastas160@gmail.com>
2021-08-20 12:30:38 +0300

    Test

---
+++ Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    public float maxHitPoints = 5;
+    public float startingHitPoints = 1;
+    public abstract void ResetCharacter();
+    public abstract IEnumerator DamageCharacter(float damage, float interval);
+    public virtual void KillCharacter()
+    {
+        //Destroy(gameObject);
+        gameObject.SetActive(false);
+    }
+    public virtual IEnumerator FlickerCharacter()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
